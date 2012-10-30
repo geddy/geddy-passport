e3a8b4ededb9682248fd209b042fcd8b6ca63ad0
mde <mde@yammer-inc.com>
2012-10-29 18:11:05 -0700

    Added controller and views for User

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -2,7 +2,11 @@ var Users = function () {
   this.respondsWith = ['html', 'json', 'xml', 'js', 'txt'];
 
   this.index = function (req, resp, params) {
-    this.respond({params: params});
+    var self = this;
+
+    geddy.model.User.all(function(err, users) {
+      self.respond({params: params, users: users});
+    });
   };
 
   this.add = function (req, resp, params) {
@@ -10,28 +14,65 @@ var Users = function () {
   };
 
   this.create = function (req, resp, params) {
-    // Save the resource, then display index page
-    this.redirect({controller: this.name});
+    var self = this
+      , user = geddy.model.User.create(params);
+
+    user.save(function(err, data) {
+      if (err) {
+        params.errors = err;
+        self.transfer('add');
+      } else {
+        self.redirect({controller: self.name});
+      }
+    });
   };
 
   this.show = function (req, resp, params) {
-    this.respond({params: params});
+    var self = this;
+
+    geddy.model.User.first(params.id, function(err, user) {
+      self.respond({params: params, user: user.toObj()});
+    });
   };
 
   this.edit = function (req, resp, params) {
-    this.respond({params: params});
+    var self = this;
+
+    geddy.model.User.first(params.id, function(err, user) {
+      self.respond({params: params, user: user});
+    });
   };
 
   this.update = function (req, resp, params) {
-    // Save the resource, then display the item page
-    this.redirect({controller: this.name, id: params.id});
+    var self = this;
+
+    geddy.model.User.first(params.id, function(err, user) {
+      user.updateAttributes(params);
+
+      user.save(function(err, data) {
+        if (err) {
+          params.errors = err;
+          self.transfer('edit');
+        } else {
+          self.redirect({controller: self.name});
+        }
+      });
+    });
   };
 
   this.destroy = function (req, resp, params) {
-    this.respond({params: params});
+    var self = this;
+
+    geddy.model.User.remove(params.id, function(err) {
+      if (err) {
+        params.errors = err;
+        self.transfer('edit');
+      } else {
+        self.redirect({controller: self.name});
+      }
+    });
   };
 
 };
 
 exports.Users = Users;
-
