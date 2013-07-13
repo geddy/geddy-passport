6795ecdea447b8f80839c7a9a372cf6d48b8097a
mde <mde@fleegix.org>
2013-07-12 17:23:55 -0700

    Added migrations for Users and Passorts

---
+++ db/migrations/00000000000001_create_users.js
@@ -0,0 +1,34 @@
+var CreateUsers = function () {
+  this.up = function (next) {
+    var def = function (t) {
+          t.column('username', 'string');
+          t.column('password', 'string');
+          t.column('familyName', 'string');
+          t.column('givenName', 'string');
+          t.column('email', 'string');
+        }
+      , callback = function (err, data) {
+          if (err) {
+            throw err;
+          }
+          else {
+            next();
+          }
+        };
+    this.createTable('users', def, callback);
+  };
+
+  this.down = function (next) {
+    var callback = function (err, data) {
+          if (err) {
+            throw err;
+          }
+          else {
+            next();
+          }
+        };
+    this.dropTable('users', callback);
+  };
+};
+
+exports.CreateUsers = CreateUsers;
