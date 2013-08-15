ae0fbc7b8b8cc3e550e6cd33f8a35858b3caf666
mde <mde@fleegix.org>
2013-08-15 14:56:54 -0700

    Redirect user to previous location after auth

--- app/helpers/passport/index.js
+++ app/helpers/passport/index.js
@@ -10,6 +10,9 @@ exports.requireAuth = function () {
   if (!(this.session.get('userId') ||
       this.name == 'Main' ||
       this.name == 'Auth')) {
+    // Record the page the user was trying to get to, will
+    // try to return them there after login
+    this.session.set('successRedirect', this.request.url);
     this.redirect('/login');
   }
 };
